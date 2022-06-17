e74fda1d01d7bd7ef766a837840d3e107ab3d090
Khristofor Kelekhsaev <consulmad@gmail.com>
2022-06-17 14:52:12 +0300

    i

---
+++ 1_1.py
@@ -0,0 +1,11 @@
+name = input('enter your name: ')
+
+age = int(input('enter your age'))
+
+weight = int(input('enter your weight: '))
+
+height = float(input('enter your height in metrs: '))
+
+BMI = (weight / height ** 2)
+
+print(BMI)
